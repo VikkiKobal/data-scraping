36d8a66393489ba3477b6853de9d4d91463e21de
Viktoriia Kobal <kobal.viktoriia@student.uzhnu.edu.ua>
2025-03-08 22:12:20 +0200

    some improvements

--- country_scraper.py
+++ country_scraper.py
@@ -1,4 +1,3 @@
-import re
 from requests import get
 from bs4 import BeautifulSoup
 
@@ -8,41 +7,39 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
 }
 
-page = get(URL, headers=HEADERS)
-soup = BeautifulSoup(page.content, "html.parser")
-table = soup.find("table", class_="wikitable")
+FILE_NAME = "countries_info.txt"
+with open(FILE_NAME, "w", encoding="utf-8") as file:
+    page = get(URL, headers=HEADERS)
+    soup = BeautifulSoup(page.content, "html.parser")
+    table = soup.find("table", class_="wikitable")
 
-clean_text = lambda text: re.sub(r'\s+', ' ', text.strip())
-
-with open("countries_and_languages.txt", "w", encoding="utf-8") as languages_file:
     for row in table.find_all("tr")[1:]:
         cells = row.find_all("td")
         country_link_tag = cells[1].find("a")
-        if country_link_tag:
-            country_name = clean_text(country_link_tag.text)
-            country_info = f"Країна: {country_name} — "
 
-            info_url = BASE_URL + country_link_tag.get("href")
-            page_info = get(info_url, headers=HEADERS)
+        if country_link_tag:
+            country_name = country_link_tag.text.strip()
+            country_url = BASE_URL + country_link_tag.get("href")
+
+            file.write(f"Країна: {country_name} — {country_url}\n")
+            print(f"Країна: {country_name} — {country_url}")
+
+            page_info = get(country_url, headers=HEADERS)
             soup_info = BeautifulSoup(page_info.content, "html.parser")
 
             infobox = soup_info.find("table", class_="infobox geography")
 
-            official_languages = ""
             if infobox:
                 rows = infobox.find_all("tr")
                 for row in rows:
                     header = row.find("th")
                     data = row.find("td")
                     if header and data:
-                        header_text = clean_text(header.get_text())
-                        data_text = clean_text(data.get_text())
+                        header_text = header.get_text(" ", strip=True)
+                        data_text = data.get_text(" ", strip=True)
 
-                        if "Офіційні мови" in header_text:
-                            official_languages = data_text
-                            break
+                        file.write(f"{header_text}: {data_text}\n")
+                        print(f"{header_text}: {data_text}")
 
-            country_info += clean_text(official_languages)
-            languages_file.write(country_info + "\n")
-
-            print(country_info)
+            file.write("\n")
+            print("\n")
